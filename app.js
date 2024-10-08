1bc0c1eb5d63c85a88665b49482b201e26ebb79f
LucasGarcia2 <lucasgarcia424@gmail.com>
2024-10-08 15:01:59 -0300

    primer commit

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-/* eslint-disable bot-whatsapp/func-prefix-dynamic-flow-await */
-const { createBot, createProvider, createFlow, addKeyword, addAnswer } = require('@bot-whatsapp/bot');
+const { createBot, createProvider, addKeyword, addAnswer, createFlow } = require('@bot-whatsapp/bot');
 const BaileysProvider = require('@bot-whatsapp/provider/baileys');
 const QRPortalWeb = require('@bot-whatsapp/portal');
 const MockAdapter = require('@bot-whatsapp/database/mock');
 const nodemailer = require('nodemailer');
+const axios = require('axios');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 require('dotenv').config();
 
-// Creamos la base de datos JSON
+// Configuración base de datos
 const adapter = new FileSync('database.json');
 const db = low(adapter);
+db.defaults({ tickets: 100 }).write();
 
-// Inicializamos la base de datos con un objeto vacío si está vacía
-db.defaults({}).write();
-
-// Configurar el transportador SMTP para enviar correos
+// Configurar correo electrónico
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -24,21 +22,14 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-// Flujo principal y saludo
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo','ola', 'ol', 'o', 'l', 'a', 'b', 'bu', 'buenas','buena', 'bue', 'wenas', 'saludos', '0', 'volver',])
-    .addAnswer('👋 ¡Bienvenido al servicio de autogestión de incidentes de *San Juan Innova S.E.*! Por favor, elige el número que corresponda a tu entidad.')
-    .addAnswer('*1)* 💼 Cliente')
-    .addAnswer('*2)* 🏫 Institución pública o educativa');
-
-// Función para enviar correo electrónico con el correo del usuario como remitente
-const enviarCorreo = async (asunto, mensaje, correoUsuario) => {
-
-    // Agrega el correo del usuario al arreglo de destinatarios
-    const destinatarios = process.env.GMAIL_RECEPTORS.split(',');
+// Función para enviar correo electrónico
+const enviarCorreo = async (asunto, mensaje) => {
+    const destinatarios = ['mauricio.aubone@sanjuaninnovase.com.ar'];
+    const remitente = process.env.GMAIL_USER;
 
     const mailOptions = {
-        from: correoUsuario,
-        to: destinatarios, // Usa el correo configurado en el archivo .env
+        from: remitente,
+        to: destinatarios,
         subject: asunto,
         text: mensaje
     };
@@ -48,135 +39,83 @@ const enviarCorreo = async (asunto, mensaje, correoUsuario) => {
         console.log('Correo electrónico enviado exitosamente');
     } catch (error) {
         console.error('Error al enviar el correo electrónico:', error);
-        throw error;
     }
 };
 
-// Flujo para clientes
-const flowCliente = addKeyword(['1', 'cliente'])
-    .addAction(async (_, { flowDynamic, state }) => {
-        // Verificar si ya se completó el flujo
-        const clienteFlowCompleted = state.clienteFlowCompleted || db.get('clienteFlowCompleted').value();
-        if (clienteFlowCompleted) {
-            // Si ya se completó el flujo, enviar el mensaje de cierre y salir del flujo
-            return await flowDynamic('Muchas gracias por usar nuestro sistema de autogestión de incidentes 🛠️. Su reclamo fue derivado al área técnica, en breve nos comunicaremos con usted.');
-        } else {
-            // Si no se ha completado el flujo, solicitar el número de cliente
-            return await flowDynamic('🔢 Por favor, ingrese su número de CUIT sin puntos ni guiones. También puedes escribir *volver* o *0* para regresar al menú anterior.');
-        }
-    })
-    .addAction({ capture: true }, async (ctx, { flowDynamic, state }) => {
-        // Capturar el número de cliente proporcionado por el usuario
-        const numeroCliente = ctx.body.trim();
+// Función para enviar mensaje a Telegram
+const enviarMensajeTelegram = async (mensaje) => {
+    const token = process.env.TELEGRAM_BOT_TOKEN;
+    const chatId = process.env.TELEGRAM_CHAT_ID;
 
-        // Verificar si el usuario quiere volver al menú anterior
-        if (numeroCliente.toLowerCase() === 'volver' || numeroCliente === '0') {
-            return await flowDynamic(flowPrincipal); // Volver al menú principal
-        }
-
-        // Verificar a quién corresponde el número de cliente
-        const cliente = obtenerNombreCliente(numeroCliente); // Función para obtener el nombre del cliente
-        if (cliente) {
-            // Cliente reconocido
-            state.cliente = cliente; // Guardar el nombre del cliente en el estado
-            db.set('cliente', cliente).write(); // Guardar el nombre del cliente en la base de datos
-            state.clienteFlowCompleted = true; // Marcar el flujo como completado en el estado
-            db.set('clienteFlowCompleted', true).write(); // Marcar el flujo como completado en la base de datos
-            
-            // Enviar mensaje de bienvenida personalizado
-            const mensajeBienvenida = `Bienvenido ${cliente} 🌐\n\nPor favor, complete este formulario 📝 (https://san-juan-innova.odoo.com/helpdesk/atencion-al-cliente-2) con sus datos para que podamos generar su reclamo. ⚠️ Por favor conserve su número de ticket que le será solicitado.\n\nTambién puede consultar el cronograma 🗓️ de la guardia aquí (https://drive.google.com/drive/folders/1UNuSopa_iPfaYNADgC7BQPXke40FZQRr). \n\n📩 Recuerde revisar su bandeja de correo para ver el estado de su reclamo. Si no encuentra el correo, revise en su bandeja de spam.`;
-            await flowDynamic(mensajeBienvenida);
-            
-            // Enviar correo con mensaje adicional
-            const asunto = `Nueva incidencia generada del cliente ${cliente}, a través de whatsapp`;
-            const mensaje = `Por favor, atender la solicitud del cliente ${cliente} CUIT nro ${numeroCliente}, recuerde revisar el ticket generado en https://san-juan-innova.odoo.com/web#action=597&active_id=2&model=helpdesk.ticket&view_type=kanban&menu_id=383&cids=1`;
-            await enviarCorreo(asunto, mensaje, ctx.from); // ctx.from contiene el correo del usuario
-            
-            // Eliminar el cliente de la base de datos al finalizar el flujo
-            state.clienteFlowCompleted = false;
-            db.unset('cliente').write();
-            db.unset('clienteFlowCompleted').write();
-
-            // Enviar mensaje de cierre
-            return await flowDynamic('Muchas gracias por usar nuestro sistema de autogestión de incidentes 🛠️. Su reclamo fue derivado al área técnica, en breve nos comunicaremos con usted.');
-        } else {
-            // Cliente no reconocido
-            return await flowDynamic('Lo siento, el número de CUIT ingresado no corresponde a ningún cliente registrado.');
-        }        
-    });
-
-// Función para obtener el nombre del cliente
-function obtenerNombreCliente(numeroCliente) {
-
-    if (numeroCliente === '20291026908') {
-        return 'Hugo Leandro Leiria';
-    } else if (numeroCliente === '30681700281') {
-        return 'Interredes S.A';
-    } else if (numeroCliente === '20355088287') {
-        return 'Facundo Javier Caselles Costa';
-    } else if (numeroCliente === '20164591388') {
-        return 'Cesar Augusto Vega';
-    } else if (numeroCliente === '20301117400') {
-        return 'Raul Omar Cortez';
-    } else if (numeroCliente === '30709457477') {
-        return 'XF Comunicaciones S.A.';
-    } else if (numeroCliente === '30708794763') {
-        return 'InterSat S.A';
-    } else if (numeroCliente === '30665234114') {
-        return 'Genneia';
-    } else if (numeroCliente === '30717517292') {
-        return 'Ethernet';
-    } else if (numeroCliente === '30707719105') {
-        return 'Netropolis';
-    } else if (numeroCliente === '30716741512') {
-        return 'Red Soft S.A.S';
-    } else if (numeroCliente === '13572468') {
-        return 'Admin';
-    } else {
-        return null; // Retorna null si el número de cliente no corresponde a ninguno reconocido
+    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+    try {
+        await axios.post(url, {
+            chat_id: chatId,
+            text: mensaje
+        });
+        console.log('Mensaje enviado a Telegram exitosamente');
+    } catch (error) {
+        console.error('Error al enviar el mensaje a Telegram:', error);
     }
-}
+};
 
-// Flujo para instituciones
-const flowInstituciones = addKeyword(['2', 'institución'])
-    .addAction(async (_, { flowDynamic, state }) => {
-        if (state.institucionFlowCompleted) {
-            return await flowDynamic('Muchas gracias por usar nuestro sistema de autogestión de incidentes 🛠️. Su reclamo fue derivado al área técnica.');
-        } else {
-            return await flowDynamic('Por favor, ingrese el número de CUE de su institución. También puedes escribir *volver* o *0* para regresar al menú principal.');
-        }
-    })
-    .addAction({ capture: true }, async (ctx, { flowDynamic, state }) => {
-        const cue = ctx.body.trim();
+const flowAsistencia = addKeyword(['1'])  // Captura la opción 1
+    .addAnswer('Ha seleccionado "Solicitar asistencia técnica". A continuación le pediremos los datos.', { delay: 1000 })
+    .addAnswer('Por favor, ingrese sus datos separados por comas: Nombre, Teléfono, Institución, Dirección, Descripción del problema.', 
+    { capture: true }, async (ctx, { flowDynamic }) => {
+        const userData = ctx.body.trim();
+        const ticketNumber = db.get('tickets').value();
+        db.set('tickets', ticketNumber + 1).write();
 
-        if (cue.toLowerCase() === 'volver' || cue === '0') {
-            return await flowDynamic(flowPrincipal);
-        }
+        // Enviar mensaje a Telegram
+        const telegramMessage = `
+Nueva solicitud de asistencia técnica:
+Ticket: #${ticketNumber}
+Datos: ${userData}
+        `;
+        await enviarMensajeTelegram(telegramMessage);
 
-        const instituciones = require('./instituciones.json');
-        const nombreInstitucion = instituciones[cue];
+        // Finaliza el flujo de asistencia y regresa al flujo principal
+        await flowDynamic([
+            `Su reclamo ha sido registrado con el ticket #${ticketNumber}. Nos pondremos en contacto en breve.`,
+            'Regresando al menú principal...'
+        ]);
 
-        if (nombreInstitucion) {
-            state.institucion = nombreInstitucion;
-            state.institucionFlowCompleted = true;
-            
-            const mensajeBienvenida = `Bienvenido ${nombreInstitucion}`;
-            await flowDynamic(mensajeBienvenida);
-
-            await flowDynamic('Por favor, complete este formulario 📝 (https://san-juan-innova.odoo.com/helpdesk/instituciones-3) con sus datos para que podamos generar su reclamo.\n\n📩 Recuerde revisar su bandeja de correo para ver el estado de su reclamo. Si no encuentra el correo, revise en su bandeja de spam.');
-
-            state.institucionFlowCompleted = false;
-
-            return await flowDynamic('Muchas gracias por usar nuestro sistema de autogestión de incidentes 🛠️. Su reclamo fue derivado al área técnica.');
-        } else {
-            return await flowDynamic('El número de CUE ingresado no corresponde a ninguna institución registrada. Por favor, intente nuevamente.');
-        }
+        return await flowDynamic(flowPrincipal);  // Regresa al flujo principal
     });
+
+    const flowNuevaConexion = addKeyword(['2'])  // Captura la opción 2
+    .addAnswer('Ha seleccionado "Solicitar nueva conexión de servicio". A continuación le pediremos los datos.', { delay: 1000 })
+    .addAnswer('Ingrese los siguientes datos separados por comas: Nombre y Apellido, Teléfono de contacto, Nombre de la institución, Dirección, Correo electrónico.', 
+    { capture: true }, async (ctx, { flowDynamic }) => {
+        const userData = ctx.body.trim();
+
+        const mailSubject = 'Solicitud de nueva conexión de servicio';
+        const mailMessage = `Nueva solicitud de conexión de servicio:\n\nDatos: ${userData}`;
+
+        await enviarCorreo(mailSubject, mailMessage);
+
+        // Finaliza el flujo de nueva conexión y regresa al flujo principal
+        await flowDynamic([
+            'Su solicitud de nueva conexión de servicio ha sido registrada. Nos pondremos en contacto en breve.',
+            'Regresando al menú principal...'
+        ]);
+
+        return await flowDynamic(flowPrincipal);  // Regresa al flujo principal
+    });
+
+    const flowPrincipal = addKeyword(['hola', 'ole', 'alo','ola', 'ol', 'o', 'l', 'a', 'b', 'bu', 'buenas','buena', 'bue', 'wenas', 'saludos', '0', 'volver'])
+    .addAnswer('¡👋 Hola soy Avi! El asistente virtual de San Juan Innova S.E.', {delay: 750})
+    .addAnswer('Por favor ingrese el número de la opción que desea:', {delay: 750})
+    .addAnswer('1) 🛠️ Solicitar asistencia técnica\n' +
+        '2) 💼 Solicitar nueva conexión de servicio', 
+        { capture: true }
+    );
 
 // Crear el bot y establecer los flujos
 const main = async () => {
     const adapterDB = new MockAdapter();
-    const adapterFlow = createFlow([flowPrincipal, flowCliente, flowInstituciones]);
+    const adapterFlow = createFlow([flowPrincipal, flowNuevaConexion, flowAsistencia]);
     const adapterProvider = createProvider(BaileysProvider);
 
     createBot({
